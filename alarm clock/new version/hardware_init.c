976cfb70a2e149cf93649480396c659cf633e577
Adeyemi Ekundayo <166171920+adeyem1Ekund4@users.noreply.github.com>
2025-06-11 21:26:47 -0400

    Create hardware_init.c

---
+++ alarm clock/new version/hardware_init.c
@@ -0,0 +1,9 @@
+#include "hardware_init.h"
+#include <xc.h>
+#include "lcd.h"
+#include "ssd.h"
+
+void Hardware_Init(void) {
+    // Set TRIS, ANSEL, etc. as in your original initialize_ports()
+    // Call LCD_Init(), SSD_Init(), etc.
+}
